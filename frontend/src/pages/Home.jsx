d01a9d3f38a4da4de74ac4b9226e16c78b33fa46
Yash Garg <143745907+yashgarg9606@users.noreply.github.com>
2025-06-04 23:13:46 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     useEffect(()=>{
       setLoading(true);
       axios
-        .get('http://localhost:5554/books')
+        .get('https://mern-stack-project-bnty.onrender.com/books')
         .then((response)=>{
           setBooks(response.data.data);
           setLoading(false)
